fbd95324676f97f3dd762fd43c682a4898aed7ae
Mika Bogaire <mikabs2003@gmail.com>
2020-12-17 12:58:49 +0200

    Initial commit

---
+++ fastgetter.py
@@ -0,0 +1,57 @@
+import requests
+import time
+import threading
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def slowgetter():
+    start = time.time()
+
+    for i in range(1, 33):
+        url = 'https://data.cyber.org.il/os/demo/movie{0}.txt'.format(i)
+        r = requests.get(url, allow_redirects=True)
+        open('movie{0}.txt'.format(i), 'wb').write(r.content)
+        file = open('movie{0}.txt'.format(i), 'r')
+        print("movie number {0} -".format(i), file.read())
+
+    end = time.time()
+    print(end - start)
+
+
+def worker(i):
+    # get movie from url
+    url = 'https://data.cyber.org.il/os/demo/movie{0}.txt'.format(i)
+    r = requests.get(url, allow_redirects=True)
+    
+    # save movie in file
+    open('movie{0}.txt'.format(i), 'wb').write(r.content)
+    
+    file = open('movie{0}.txt'.format(i), 'r')
+
+    # printing using logging
+    logging.info("movie number {0} - {1}".format(i, file.read()))
+
+
+def fastgetter():
+    threads = []
+    start = time.time()
+
+    # creats threads
+    for i in range(1, 33):
+        t = threading.Thread(target=worker, args=(i,))
+        threads.append(t)
+        t.start()
+
+    # waits for all threads to finish
+    for i in threads:
+        i.join()
+        
+    end = time.time()
+    logging.info("time:{}".format(end - start))
+
+
+if __name__ == "__main__":
+    # slowgetter()
+    fastgetter()
